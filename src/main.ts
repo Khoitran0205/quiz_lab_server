6d14663961224b5fe39ffa3c013ce047cf4fbc57
Khoi Tran <duykhoi96tn@gmail.com>
2024-04-21 23:41:54 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.APP_PORT || 3001, () => {
+  await app.listen(process.env.APP_PORT || 3001, '0.0.0.0', () => {
     console.log(`App listening in ${process.env.APP_PORT || 3001}`);
   });
 }
