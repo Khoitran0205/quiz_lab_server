b4c4b3a76964b382aa2e04335054665248a76d1b
Khoi Tran <duykhoi96tn@gmail.com>
2024-05-01 16:20:47 +0700

    update user rank

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -71,7 +71,7 @@ export class RoomsController {
   }
 
   @ApiOperation({ summary: 'Update rank of a room' })
-  @Patch('end-room/:id')
+  @Patch('update-user-rank/:id')
   async updateRankOfARoom(@Req() req, @Param('id') id: string) {
     // const { id: updatedBy } = req?.user;
     const data = await this.roomsService.updateRankOfARoom(id);
